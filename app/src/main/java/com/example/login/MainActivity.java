251c297768ea928b6c5b0e20706b99365221ca24
andres <andresgeo96@hotmail.com>
2019-10-22 18:44:59 -0500

    ventanas de login

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText usuario;
+    private EditText password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        usuario = (EditText)findViewById(R.id.user);
+        password = (EditText)findViewById(R.id.password);
+    }
+
+    public void ingresar(View view){
+        Intent i = new Intent(this,actividad1.class);
+        i.putExtra("nombre",usuario.getText().toString());
+        startActivity(i);
+    }
+
+    public void validar(){
+        try{
+
+            String usu = usuario.getText().toString();
+            String pass= password.getText().toString();
+
+            if(usu.equals("andresgeo96@hotmail.com") && pass.equals("pass1234")){
+                Intent i = new Intent(this,actividad1.class);
+                i.putExtra("nombre",usuario.getText().toString());
+                startActivity(i);
+            }else{
+                Intent i = new Intent(this,actividad2.class);
+            }
+        }catch (Exception e){
+
+        }
+
+    }
+
+
+}
